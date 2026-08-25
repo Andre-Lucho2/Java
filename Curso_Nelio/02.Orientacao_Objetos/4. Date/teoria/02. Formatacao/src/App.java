9e54f74aa81bac2d0f612cc9b72551750ced7f0c
Andre-Lucho2 <andre.l1825@gmail.com>
2026-08-25 19:35:05 -0300

    Atualizado em 25/08/2026 v.02

--- Curso_Nelio/02.Orientacao_Objetos/4. Date/teoria/02. Formatacao/src/App.java
+++ Curso_Nelio/02.Orientacao_Objetos/4. Date/teoria/02. Formatacao/src/App.java
@@ -16,6 +16,7 @@ public class App {
         DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
         DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
         DateTimeFormatter fmt = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG);
+        DateTimeFormatter fmt6 = DateTimeFormatter.ISO_DATE;
 
         LocalDate date = LocalDate.now();
         LocalDate date01 = LocalDate.parse("2026-08-07");
@@ -26,25 +27,26 @@ public class App {
         // Formatando uma data passada em ISO
         // ------------------------------------
         // System.out.println(date01.format(fmt1));
-        System.out.println(date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-        System.out.println(date01.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-
+        // System.out.println(date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
         // System.out.println(date01.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
         // // ou
         // System.out.println(fmt1.format(date01));
 
         // System.out.println(date02.format(fmt2));
 
         DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
-        System.out.println(fmt3.format(date03));
+        // System.out.println(fmt3.format(date03));
         // obs1:
         // Como estamos formatando o Objeto Instant - Data Global, devo ter uma TimeZone
         // --> método .withZone();
         // ZoneId.systemDefault() --> hora do sistema
         // obs2: Instant não tem metodo .format --> chamá-lo atraves do fmt3
 
-        System.out.println(date02.format(fmt4));
-        System.out.println(fmt5.format(date03));
+        // System.out.println(date02.format(fmt4));
+        // System.out.println(fmt5.format(date03));
+
+        System.out.println(date01.format(fmt1));
 
     }
 }
--- Curso_Nelio/02.Orientacao_Objetos/8. Tratamento_Excecoes/teoria/src/application/App.java
+++ Curso_Nelio/02.Orientacao_Objetos/8. Tratamento_Excecoes/teoria/src/application/App.java
@@ -1,25 +1,64 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
+        // method01();
+        // System.out.println("End of program");
 
+        methodFile();
+
+    }
+
+    public static void method01() {
+        System.out.println("***METHOD1 START***");
+        method02();
+        System.out.println("***METHOD1 END***");
+    }
+
+    public static void method02() {
+        System.out.println("***METHOD2 START***");
+        Scanner sc = new Scanner(System.in);
         try {
             String[] vect = sc.nextLine().split(" ");
             int position = sc.nextInt();
             System.out.println(vect[position]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid position!");
+            e.printStackTrace();// print do rastreamento da exceção
+            sc.next();
         } catch (InputMismatchException e) {
             System.out.println("Input error");
         }
-        System.out.println("End of program");
-
         sc.close();
-
+        System.out.println("***METHOD2 END***");
     }
+
+    public static void methodFile() {
+        File file = new File("/home/deco-dev/Documentos/Metroidvania.md");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+            System.out.println();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+            System.out.println("Bloco Finally executado");
+
+        }
+    }
+
 }
